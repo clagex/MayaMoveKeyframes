44362437d8b1fb2b9bb4f06a1283b1a9823be8f8
clagex <clagex52@gmail.com>
2022-08-30 13:26:43 +1200

    implemented batch option
    allowed to read in multiple fbx objects to move the keyframes, and export them as fbx

--- MayaMoveAnim.py
+++ MayaMoveAnim.py
@@ -1,4 +1,28 @@
+import maya.mel as mel
 import maya.cmds as cmds
+import pymel.core as pm
+import glob
+import string
+
+rootDir = cmds.workspace( q=True, rd=True )
+
+def singleConvert():
+    selectAllJoints()
+    moveTimeData()
+
+def batchConvert():
+    fbxDir = rootDir + '/assets/inputs/'
+    fbxs = glob.glob(fbxDir+'*.fbx')
+
+    for fbx in fbxs:
+        fbxName = cmds.file(fbxs[1], o = True, force=True)
+        cmds.select('Tehau_Root')
+        singleConvert()
+        outName = fbxName.replace('inputs', 'outputs')
+        cmds.FBXExport('-file', outName, '-s')
+        cmds.delete('*_Root')
+
+
 
 def moveTimeData():
     objs = cmds.ls(selection=True)
@@ -13,8 +37,9 @@ def moveTimeData():
             if (numKeyframes > 0):
                 times = cmds.keyframe(attribute, query=True, index=(0,numKeyframes), timeChange=True)
                 cmds.keyframe(edit=True, relative=True, timeChange=-times[0])
+                newTimes = cmds.keyframe(attribute, query=True, index=(0,numKeyframes), timeChange=True)
 
-                
+
 def selectAllJoints():
     root = cmds.ls(selection=True)[0]
     joints = cmds.listRelatives(root, ad=True, type='joint')
@@ -22,5 +47,4 @@ def selectAllJoints():
         joints.append(root)
     cmds.select(joints)
 
-selectAllJoints()
-moveTimeData()
+batchConvert()
--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 a script to find the start time of a skeleton animation and move them to time 0
 
 ## How to Use
+#### Single Convert
 First, select your skeleton root, then run the python script
+When running the script, change the `batchConvert()` at line 50 to `singleConvert()`
+
+#### Batch Convert
+In the script, the path is set from the project root directory.<br>
+All the fbx files waiting for converting is in `rootDirectory/assets/input`<br>
+The converted fbx will be written to `rootDirectory/assets/output` with the same name<br>
+
+After the files are in the folder, can just run the script!
